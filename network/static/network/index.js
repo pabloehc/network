60fd28c435c4ae12c9bc166e478eba77a190c184
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-07-28 21:14:48 -0400

    shows lastest posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-
+    load_posts();
 
 });
+
+function load_posts() {
+    fetch("/posts")
+    .then(response => response.json())
+    .then(posts => {
+        
+        posts.forEach(post => {
+            const user = post.user;
+            const content = post.content;
+            const timestamp = post.timestamp;
+
+            const div = document.createElement('div');
+            div.className = 'post';
+            div.innerHTML = `<p>${user}</p>
+                            <p>${timestamp}</p>
+                            <p>${content}</p>`;
+            document.querySelector('.body').append(div);
+        })
+    })
+}
