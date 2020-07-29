2f0c052e60c1e33c11cf279831424e6c2ec67ed4
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-07-28 20:24:54 -0400

    writing a post saves it the post to the database

--- network/models.py
+++ network/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    posts = models.ManyToManyField('Post', blank=True, related_name='user')
-    following = models.ManyToManyField(User, blank=True, related_name='followers')
+    posts = models.ManyToManyField('Post', blank=True, related_name='owner')
+    following = models.ManyToManyField('User', blank=True, related_name='followers')
 
 class Post(models.Model):
     time = models.DateField(auto_now=True)
--- network/views.py
+++ network/views.py
@@ -3,8 +3,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
@@ -61,3 +62,15 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        user = request.user
+        content = request.POST['content']
+
+        p = Post(content=content)
+        p.save()
+        p.owner.add(user)
+
+    return HttpResponseRedirect(reverse("index"))
